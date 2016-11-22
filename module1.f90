2bfffb40188a731ed65290d17ab85b0f00ac4db4
Kevin Speyer <speyer.kevin@gmail.com>
2016-11-22 19:16:25 -0300

    New outputs added: Rend-to-end and R_cm

--- module1.f90
+++ module1.f90
@@ -3,7 +3,7 @@ module com_vars
 implicit none
 integer :: i_mon, j_mon, n_mon, i_time, n_time, i_dim, n_dim, mv_mon
 real(kind=8) , dimension(:,:), allocatable :: r0
-real(kind=8) , dimension(:), allocatable :: boundary, dr_mv
-real(kind=8) :: Temp, a_box, Lx, Ly, Lz, energy, delta_energy, old_energy, k_spr
+real(kind=8) , dimension(:), allocatable :: boundary, dr_mv, r_cm, r_end
+real(kind=8) :: Rend2, Temp, a_box, Lx, Ly, Lz, energy, delta_energy, old_energy, k_spr, inv_nmon
 end module com_vars
  
